1ca8fa96d96d23616616c77c331561bc306560bf
Katy Stadler <100023534@mvla.net>
2024-02-01 12:36:38 -0800

    fixed reversed() method

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedActual() {
+    int[] input1 = {1, 2, 3};
+    int[] actual = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{3, 2, 1}, actual);
+  }
+
+  @Test
+  public void testReversedLongList() {
+    int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int[] expected = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int[] actual = ArrayExamples.reversed(input);
+    assertArrayEquals(expected, actual);
+  }
 }
